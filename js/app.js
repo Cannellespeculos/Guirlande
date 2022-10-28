631ec0ceb5cf13c84bfba3c46732fabf3103554a
Ilyansm <cjeanpierre@edenschool.fr>
2022-10-28 14:48:57 +0200

    Guirlande

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let rouge = document.getElementById("rouge")
+let vert = document.getElementById("vert")
+let bleu = document.getElementById("bleu")
+let jaune = document.getElementById("jaune")
+
+rouge.addEventListener("click",)
